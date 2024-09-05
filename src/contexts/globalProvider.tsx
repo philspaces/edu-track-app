ae893724a1bdb119d894205e77a27c270b79bd63
evansnguyen <long.np68@gmail.com>
2024-09-05 18:05:07 +0800

    feat: add subscriptions to real-time events

--- src/contexts/globalProvider.tsx
+++ src/contexts/globalProvider.tsx
@@ -1,5 +1,8 @@
-import {createContext, useContext, useState} from "react";
+import {createContext, useContext, useEffect, useState} from "react";
 import authContext from "./authContext.tsx";
+import {useAmplifyClient} from "./amplifyClientContext.tsx";
+import * as subscriptions from "../graphql/subscriptions.js";
+import toast from "react-hot-toast";
 
 export interface IGlobal {
     isLoading?: boolean,
@@ -20,6 +23,7 @@ const defaultState = {
 export const GlobalContext = createContext<IGlobal>(defaultState);
 const GlobalProvider = ({children}) => {
     const {currentUser: user} = authContext
+    const amplifyClient = useAmplifyClient();
 
     const [isLoading, setIsLoading] = useState(false);
     const [modal, setModal] = useState(false);
@@ -31,6 +35,54 @@ const GlobalProvider = ({children}) => {
     const closeModal = () => {
         setModal(false);
     };
+
+    // users mutation subscriptions
+    useEffect(() => {
+        const createSub = amplifyClient
+            .graphql({query: subscriptions.onCreateStudent})
+            .subscribe({
+                next: ({data}) => {
+                    toast.success('New student added!')
+                    console.log(data)
+                },
+                error: (error) => {
+                    toast.error("Something went wrong")
+                    console.warn(error)
+                }
+            });
+
+        const updateSub = amplifyClient
+            .graphql({query: subscriptions.onUpdateStudent})
+            .subscribe({
+                next: ({data}) => {
+                    toast.success('Update successfully!')
+                    console.log(data)
+                },
+                error: (error) => {
+                    toast.error("Something went wrong")
+                    console.warn(error)
+                }
+            });
+
+        const deleteSub = amplifyClient
+            .graphql({query: subscriptions.onDeleteStudent})
+            .subscribe({
+                next: ({data}) => {
+                    toast.success('Delete successfully!')
+                    console.log(data)
+                },
+                error: (error) => {
+                    toast.error("Something went wrong")
+                    console.warn(error)
+                }
+            });
+
+        return () => {
+            createSub.unsubscribe();
+            updateSub.unsubscribe();
+            deleteSub.unsubscribe();
+        }
+    }, []);
 
     const state: IGlobal = {
         isLoading,
